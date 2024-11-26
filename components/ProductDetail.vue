346bdb9990b691a22ca2d8e70597900146bf229a
Leo-Pellegrin <leo.pellegrin87@gmail.com>
2024-11-26 00:02:01 -0500

    Ajout image product + debut js images et grid images

--- components/ProductDetail.vue
+++ components/ProductDetail.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="min-h-screen flex items-center justify-center">
-    <div class="container mx-auto grid grid-cols-2">
-      <div class="grid grid-rows-9 border-2 border-red-500">
-        <UCarousel v-slot="{ productimage }" :items="productimages" :ui="{ item: 'basis-full' }"
-          class="rounded-lg overflow-hidden" arrows>
-          <img :src="productimage" class="w-full" draggable="false">
-        </UCarousel>
+    <div class="imageContainer mx-auto">
+      <!-- Main image -->
+      <div class="container mx-auto">
+        <div class="border-2 border-red-500 rounded-lg">
+          <img :src="imageSelected" class="w-full max-w-[450px] mx-auto" draggable="false">
+        </div>
       </div>
+      <!-- Subimages -->
+      <div class="grid grid-cols-4 mt-8 gap-6">
+        <div v-for="(thumbnail, index) in productthumbnails" :key="index" class="mx-auto flex-none max-w-[95px]">
+          <img :src="thumbnail.path" class="w-full border-red-500 rounded-lg" @click="imageSelection(index)" />
+        </div>
+      </div>
+
     </div>
+
     <div class="flex flex-col">
-      <div>
+      <div class="text-xl">
         Sneaker Company
       </div>
-      <div>
+      <div class="text-4xl">
         Fall Limited Edition Sneakers
       </div>
       <div>
@@ -27,30 +35,62 @@
 
 const page = ref(null);
 
+
+
 const productimages = ref([
   '/image-product-1.jpg',
   '/image-product-2.jpg',
   '/image-product-3.jpg',
-  '/image-product-4.jpg'
+  '/images/image-product-4.jpg'
+])
+
+const imageSelected = ref('')
+
+const productimagestest = ref([
+  'https://picsum.photos/600/600?random=1',
+  'https://picsum.photos/600/600?random=2',
+  'https://picsum.photos/600/600?random=3',
+  'https://picsum.photos/600/600?random=4',
+  'https://picsum.photos/600/600?random=5',
+  'https://picsum.photos/600/600?random=6'
 ])
 
 const productthumbnails = ref([
-  '/images/image-product-1-thumbnail.jpg',
-  '/images/image-product-2-thumbnail.jpg',
-  '/images/image-product-3-thumbnail.jpg',
-  '/images/image-product-4-thumbnail.jpg'
+  {
+    path: '/image-product-1-thumbnail.jpg',
+    selected: false
+  },
+  {
+    path: '/image-product-2-thumbnail.jpg',
+    selected: false
+  },
+  {
+    path: '/image-product-3-thumbnail.jpg',
+    selected: false
+  },
+  {
+    path: '/image-product-4-thumbnail.jpg',
+    selected: false
+  }
 ])
 
+function imageSelection(index: number){
+  productthumbnails.value[index].selected = !productthumbnails.value[index].selected;
+  imageSelected.value = productimages.value[index]
+}
 
 async function fetchData() {
 }
 
 onMounted(() => {
-  fetchData();
-  console.log(page.value)
+  imageSelected.value = productimages.value[0]
 })
 
 
+
+function thumbnail(value: { path: string; selected: boolean; }, index: number, array: { path: string; selected: boolean; }[]): unknown {
+  throw new Error('Function not implemented.');
+}
 </script>
 
 <style></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/content"]
+  modules: ["@nuxt/ui", "@nuxt/content"],
+  compatibilityDate: "2024-11-25"
 })
